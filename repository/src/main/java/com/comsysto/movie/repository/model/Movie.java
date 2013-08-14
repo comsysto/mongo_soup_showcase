00795f0eaf7b1a2d12423d9387d439b7ad9fb7cf
Elisabeth Engel <Elisabeth.Engel@comsysto.com>
2013-08-14 14:43:31 +0200

    changed mongodb name 2

--- repository/src/main/java/com/comsysto/movie/repository/model/Movie.java
+++ repository/src/main/java/com/comsysto/movie/repository/model/Movie.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
  */
 @Document(collection = Movie.COLLECTION_NAME)
 public class Movie implements Serializable {
-    public static final String COLLECTION_NAME = "cc_MTUWzOgBOtqt";
+    public static final String COLLECTION_NAME = "movie";
 
     @Id
     private ObjectId id;
