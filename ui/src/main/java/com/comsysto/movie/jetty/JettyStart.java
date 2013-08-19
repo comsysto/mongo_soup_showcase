500f6b494e0754d88f1f65f3a62a98c2ab80af0d
Elisabeth Engel <Elisabeth.Engel@comsysto.com>
2013-08-19 15:26:57 +0200

    Gradle task stage completed

--- ui/src/main/java/com/comsysto/movie/jetty/JettyStart.java
+++ ui/src/main/java/com/comsysto/movie/jetty/JettyStart.java
@@ -6,12 +6,15 @@ public class JettyStart {
 
         int port;
 
+        // if a parameter is given to this method
         if (args.length > 0) {
             port = Integer.valueOf(args[0]);
         }
+        // if there is a system parameter
         else if (!System.getenv("PORT").isEmpty()) {
             port = Integer.parseInt(System.getenv("PORT"));
         }
+        // default
         else {
             port = 8080;
         }
