d29565e72ac6ec66e7dab3f5086dffe1357165f8
Elisabeth Engel <Elisabeth.Engel@comsysto.com>
2013-08-19 16:13:17 +0200

    cloudControl flag in JettyStart

--- ui/src/main/java/com/comsysto/movie/jetty/JettyStart.java
+++ ui/src/main/java/com/comsysto/movie/jetty/JettyStart.java
@@ -6,33 +6,38 @@ public class JettyStart {
 
         int port;
         String activeSpringProfile;
-
-        // if a parameter is given to this method
-        if (args.length > 0) {
-            port = Integer.valueOf(args[0]);
-        }
-        // if there is a system parameter
-        else if (!System.getenv("PORT").isEmpty()) {
-            port = Integer.parseInt(System.getenv("PORT"));
-        }
-        // default
-        else {
-            port = 8080;
-        }
-
-        System.out.println("JettyStart <" + port + ">");
+        boolean cloudControlFlag = true;
 
 
-
-        if (!System.getenv("CRED_FILE").isEmpty()) {
+        if (cloudControlFlag) {
             activeSpringProfile = "cloudControl";
 
-        }  else {
+            if (!System.getenv("PORT").isEmpty()) {
+                port = Integer.parseInt(System.getenv("PORT"));
+            }
+            // default
+            else {
+                port = 8080;
+            }
+
+        } else {
             activeSpringProfile = "local";
+
+            // if a parameter is given to this method
+            if (args.length > 0) {
+                port = Integer.valueOf(args[0]);
+            }
+            // default
+            else {
+                port = 8080;
+            }
         }
 
         System.setProperty("spring.profiles.active", activeSpringProfile);
         System.out.println("Spring profile <" + activeSpringProfile + ">");
+
+
+        System.out.println("JettyStart <" + port + ">");
 
         JettyStarterApplication jettyStarter = new JettyStarterApplication(port);
         jettyStarter.startServer();
